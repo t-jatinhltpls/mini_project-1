144a88b7ee490d82d8ff6b8fe013edfc61d88b84
jatinKumar-k <65779938+jatinKumar-k@users.noreply.github.com>
2023-03-17 18:40:32 +0530

    Update final1.java

--- final1.java
+++ final1.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParserConfiguration;
@@ -15,11 +17,14 @@ import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeS
 public class final1 {
     public static void main(String[] args) {
         // Set the directory path
-        String directoryPath = "C:\\Users\\91829\\Desktop\\mini_project\\input1";
+        String directoryPath = "C:\\Users\\91829\\Desktop\\mini_project\\input\\";
         // Set the output file path
-        String outputFilePath = "C:\\Users\\91829\\Desktop\\mini_project\\output\\output.txt";
+        String outputFilePath = "C:\\Users\\91829\\Desktop\\mini_project\\output\\";
 
-        try (FileWriter fileWriter = new FileWriter(outputFilePath)) {
+        try {
+        	 FileWriter fileWriter = new FileWriter(outputFilePath+"output.txt");
+        	 Set<String> set = new HashSet<String>();
+        	 
             // Create a new ParserConfiguration instance and set a symbol resolver
             ParserConfiguration parserConfiguration = new ParserConfiguration();
             parserConfiguration.setSymbolResolver(new JavaSymbolSolver(new ReflectionTypeSolver()));
@@ -30,6 +35,8 @@ public class final1 {
             // Get all files in the directory
             File directory = new File(directoryPath);
             File[] files = directory.listFiles();
+            
+            
 
             // Loop through all files in the directory
             for (File javaFile : files) {
@@ -52,31 +59,61 @@ public class final1 {
                                 try {
                                     String fullyQualifiedName = methodCall.resolve().getQualifiedName();
                                     // Write the fully qualified name to the output file
-                                    fileWriter.write(fullyQualifiedName + "\n");
+                                    if (fullyQualifiedName.startsWith("java")) {
+                                        set.add(fullyQualifiedName);
+                                        File methodFolder = new File(outputFilePath+fullyQualifiedName);
+                                        if(!methodFolder.exists()) {
+                                        	methodFolder.mkdir();
+                                        }
+                                  	    System.out.println(fullyQualifiedName);
+                                  	    String newDataPointFile = outputFilePath+fullyQualifiedName+"\\"+method.getName()+".java";
+                                  	    System.out.println(newDataPointFile);
+                                  	    writeFile(newDataPointFile, method.toString());  
+                                    }
                                 } catch (UnsolvedSymbolException e) {
                                     // Handle the exception
-                                    System.out.println("Could not resolve method call: " + methodCall);
+//                                    System.out.println("Could not resolve method call: " + methodCall);
                                 }
                                 catch ( UnsupportedOperationException e){
-                                	 System.out.println("UnsupportedOperationException for:  " + methodCall);
+//                                	 System.out.println("UnsupportedOperationException for:  " + methodCall);
                                 }
                                 
                                 catch (java.lang.RuntimeException e) {
-                                	System.out.println("java.lang.RuntimeException for: "+method.getSignature());
+//                                	System.out.println("java.lang.RuntimeException for: "+method.getSignature());
                                 }
                                 
                                 
                             }
                         }
+                        
+                        
+                        
                     } catch (IOException e) {
                         // Handle the exception
-                        System.out.println("Error parsing file: " + javaFile.getName());
+//                        System.out.println("Error parsing file: " + javaFile.getName());
                     }
                 }
             }
+            for (String str : set) {
+                fileWriter.write(str + "\n");            
+            }
+            fileWriter.close();
+            
         } catch (IOException e) {
             // Handle the exception
             System.out.println("Error writing to output file.");
         }
+        
+        
     }
+    
+    private static void writeFile(String filename, String contents) {
+        try (java.io.FileWriter writer = new java.io.FileWriter(filename)) {
+          writer.write(contents);
+          writer.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        
+      }
 }
